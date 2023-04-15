679c5072161427ca6521644a1bb923a9707325c3
FranciscoFurt <114479066+FranciscoFurt@users.noreply.github.com>
2023-04-15 19:40:34 +0100

    Add files via upload

---
+++ projeto/projeto/Client1/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Client1 {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtMensagem = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPorto = new System.Windows.Forms.TextBox();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.btEnviarMensagem = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtConsola = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtMensagem
+            // 
+            this.txtMensagem.Location = new System.Drawing.Point(138, 78);
+            this.txtMensagem.Multiline = true;
+            this.txtMensagem.Name = "txtMensagem";
+            this.txtMensagem.Size = new System.Drawing.Size(488, 64);
+            this.txtMensagem.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Comunicar com o Servidor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mensagem:";
+            // 
+            // txtPorto
+            // 
+            this.txtPorto.Location = new System.Drawing.Point(474, 168);
+            this.txtPorto.Name = "txtPorto";
+            this.txtPorto.Size = new System.Drawing.Size(152, 22);
+            this.txtPorto.TabIndex = 4;
+            this.txtPorto.Text = "50001";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(138, 168);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(152, 22);
+            this.txtIP.TabIndex = 5;
+            this.txtIP.Text = "127.0.0.1";
+            // 
+            // btEnviarMensagem
+            // 
+            this.btEnviarMensagem.Location = new System.Drawing.Point(645, 77);
+            this.btEnviarMensagem.Name = "btEnviarMensagem";
+            this.btEnviarMensagem.Size = new System.Drawing.Size(124, 65);
+            this.btEnviarMensagem.TabIndex = 6;
+            this.btEnviarMensagem.Text = "Enviar";
+            this.btEnviarMensagem.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(103, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "IP:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(414, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Porto:";
+            // 
+            // txtConsola
+            // 
+            this.txtConsola.Location = new System.Drawing.Point(31, 274);
+            this.txtConsola.Multiline = true;
+            this.txtConsola.Name = "txtConsola";
+            this.txtConsola.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtConsola.Size = new System.Drawing.Size(738, 185);
+            this.txtConsola.TabIndex = 29;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(26, 246);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 25);
+            this.label5.TabIndex = 30;
+            this.label5.Text = "Consola";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(645, 170);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 65);
+            this.button1.TabIndex = 31;
+            this.button1.Text = "Sair";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(788, 480);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtConsola);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btEnviarMensagem);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.txtPorto);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMensagem);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cliente";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMensagem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPorto;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Button btEnviarMensagem;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtConsola;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ projeto/projeto/Servidor/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servidor {
+    class Program {
+        //Criar novamente uma constante, tal como feito do lado do cliente.
+        private const int PORT = 10000;
+
+        static void Main(string[] args) {
+            // Definição das variáveis na função principal
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, PORT);
+            TcpListener listener = new TcpListener(endPoint);
+
+            // Iniciar o listener; apresentação da primeira mensagem na linha de comandos e inicialização do contador.
+            listener.Start();
+            Console.WriteLine("SERVIDOR PRONTO");
+            int clientCounter = 0;
+
+            //Criação do ciclo infinito de forma a que este esteja sempre em execução até ordem em contrário
+            while (true) {
+                // Definição da variável client do tipo TcpClient
+                TcpClient client = listener.AcceptTcpClient();
+
+                // Incrementação do contador, de forma a que vá sempre somando 1 (+1)
+                clientCounter++;
+
+                //Apresentação da mensagem indicativa do nº do client na linha de comandos
+                Console.WriteLine("Client {0} connected", clientCounter);
+
+                //Definição da variável clientHandler do tipo ClientHandler
+                ClientHandler clientHandler
+                    = new ClientHandler(client, clientCounter);
+                clientHandler.Handle();
+            }
+        }
+    }
+
+    class ClientHandler {
+
+    }
+}
+
